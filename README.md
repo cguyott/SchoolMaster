a005d3c0707130315519444fc8238bfb6aa3d534
Chris Guyott <c.guyott@gmail.com>
2020-12-15 19:08:08 -0500

    SM-1 - Created SQL database project and added Person table.

--- README.md
+++ README.md
@@ -1,2 +1,4 @@
 # SchoolMaster
 Restful web API for managing teachers, classes, students, etc.
+
+See the "School Master.docx" design document located in the "Design" folder for more information.
---
+++ Source/SchoolMaster/SQLDatabase/Tables/Person.sql
@@ -0,0 +1,16 @@
+﻿CREATE TABLE [dbo].[Person]
+(
+	[Id] INT NOT NULL IDENTITY(1,1),
+	[Prefix] NVARCHAR(6) NULL, 
+    [FirstName] NVARCHAR(50) NOT NULL, 
+    [MiddleName] NVARCHAR(50) NULL, 
+    [LastName] NVARCHAR(50) NOT NULL, 
+    [Suffix] NVARCHAR(6) NULL, 
+    [Login] NVARCHAR(64) NOT NULL, 
+    [LastLoginDate] DATETIME2 NOT NULL, 
+    [PasswordHash] NVARCHAR(30) NOT NULL, 
+    [PasswordSalt] NVARCHAR(30) NOT NULL, 
+    [LastPasswordChangedDate] DATETIME2 NOT NULL, 
+    [CreatedDate] DATETIME2 NOT NULL, 
+    CONSTRAINT [PK_Person] PRIMARY KEY ([Id]),
+)
