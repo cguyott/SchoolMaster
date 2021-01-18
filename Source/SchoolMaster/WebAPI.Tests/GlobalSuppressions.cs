d2dbc589f369a6ebe28aa770aa4b974a75e537f0
Chris Guyott <c.guyott@gmail.com>
2021-01-18 13:48:52 -0500

    SM-11 - Added email unit tests.

--- Source/SchoolMaster/WebAPI.Tests/GlobalSuppressions.cs
+++ Source/SchoolMaster/WebAPI.Tests/GlobalSuppressions.cs
@@ -8,3 +8,5 @@ using System.Diagnostics.CodeAnalysis;
 [assembly: SuppressMessage("StyleCop.CSharp.NamingRules", "SA1308:Variable names should not be prefixed", Justification = "We do use m_, s_, c_, etc. to provide additional meta information.")]
 [assembly: SuppressMessage("StyleCop.CSharp.ReadabilityRules", "SA1101:Prefix local calls with this", Justification = "We don't believe in littering our code with 'this'.")]
 [assembly: SuppressMessage("StyleCop.CSharp.LayoutRules", "SA1512:Single-line comments should not be followed by blank line", Justification = "It's fine to have a blank line after single line comments.")]
+[assembly: SuppressMessage("StyleCop.CSharp.NamingRules", "SA1303:Const field names should begin with upper-case letter", Justification = "Private const field names do not have to begin with uppercase letters.")]
+[assembly: SuppressMessage("StyleCop.CSharp.NamingRules", "SA1310:Field names should not contain underscore", Justification = "Private field names can contain underscores.")]
